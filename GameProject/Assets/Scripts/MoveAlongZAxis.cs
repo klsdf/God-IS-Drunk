f2178c3ff7cd1ae0fc6bc7c9380391a23309c8aa
闫辰祥 <1796655849@qq.com>
2025-04-12 13:29:06 +0800

    临时资产更新

---
+++ GameProject/Assets/Scripts/MoveAlongZAxis.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MoveAlongZAxis : MonoBehaviour
+{
+    public float speed = 5.0f; // 移动速度
+
+    void Update()
+    {
+        // 每帧沿世界坐标系的z轴正方向移动
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+    }
+}
