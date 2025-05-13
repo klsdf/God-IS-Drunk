ae96edb0b4091545b1018a95597506157321045d
闫辰祥 <1796655849@qq.com>
2025-05-14 00:37:58 +0800

    新手面板

--- GameProject/Assets/Scripts/UI/常驻ui提示.cs
+++ GameProject/Assets/Scripts/UI/常驻ui提示.cs
@@ -3,10 +3,16 @@ using TMPro;
 using UnityEngine;
 public class 常驻ui提示 : MonoBehaviour
 {
-    public TMP_Text text;
+    public TMP_Text 免责文本;
+
+    public TMP_Text 教程文本;
+
+
+
 
     private void Start()
     {
-        text.text = YanGF.Localization.Translate("本游戏不提倡酒后驾车、抽烟等不良行为。游戏内容请勿模仿。");
+        免责文本.text = YanGF.Localization.Translate("本游戏不提倡酒后驾车、抽烟等不良行为。游戏内容请勿模仿。");
+        教程文本.text = YanGF.Localization.Translate("1.使用鼠标点击ui开始游戏\n2.使用手柄来控制玩家移动\n3.推荐使用陀螺仪来操作玩家");
     }
 }
