c133f612a2dc99f1da1ef02de145cc833500cd61
TiefseeMatch <tiefseematch@gmail.com>
2025-05-14 14:02:58 +0800

    感谢名单

--- GameProject/Assets/Scripts/Configs/鸣谢名单.cs
+++ GameProject/Assets/Scripts/Configs/鸣谢名单.cs
@@ -2,8 +2,11 @@ public static class 鸣谢名单
 {
     public static string[] 鸣谢名单列表 = new string[]
     {
-        "鸣谢名单1",
-        "鸣谢名单2",
-        "鸣谢名单3"
+        "Thanks to：Music——Ovanisound",
+		"Thanks to：Music——Fanatical",
+		"Thanks to：Music——魔王魂",
+		"Thanks to：Music——What is love",
+        "Thanks to：Art——MeiMei",
+		"Thanks to：Art——肉山每天都在sancheck"
     };
 }
